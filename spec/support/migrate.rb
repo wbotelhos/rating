de3ac30c2458ac7917a187447842197e8747ea51
Washington Botelho <wbotelhos@gmail.com>
2018-01-14 22:42:53 -0200

    fix: spec tables creator path

--- spec/support/migrate.rb
+++ spec/support/migrate.rb
@@ -2,6 +2,8 @@
 
 require File.expand_path('../../lib/generators/rating/templates/db/migrate/create_rating_tables.rb', __dir__)
 
+Dir[File.expand_path('db/migrate/*.rb', __dir__)].each { |file| require file }
+
 CreateArticlesTable.new.change
 CreateAuthorsTable.new.change
 CreateCategoriesTable.new.change
