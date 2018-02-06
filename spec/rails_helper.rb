79586640683a2c9b1a1504562c49f5ac674598d2
Washington Botelho <wbotelhos@gmail.com>
2018-02-06 18:05:20 -0200

    fix: drop if table exists

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,8 +9,8 @@ require 'rating'
 
 client = Mysql2::Client.new(host: :localhost, username: :root)
 
-client.query 'DROP DATABASE rating_test;'
-client.query 'CREATE DATABASE rating_test;'
+client.query 'DROP DATABASE IF EXISTS rating_test;'
+client.query 'CREATE DATABASE IF NOT EXISTS rating_test;'
 
 ActiveRecord::Base.establish_connection adapter: :mysql2, database: :rating_test, username: :root
 
