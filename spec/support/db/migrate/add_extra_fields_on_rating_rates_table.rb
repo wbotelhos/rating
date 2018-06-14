0400bf8fda807f85f3c7c83bd565216d6827d27c
Washington Botelho <wbotelhos@gmail.com>
2018-06-14 14:19:19 -0300

    rm: removes remove index command
    keeps all scopes together

--- spec/support/db/migrate/add_extra_fields_on_rating_rates_table.rb
+++ spec/support/db/migrate/add_extra_fields_on_rating_rates_table.rb
@@ -5,6 +5,6 @@ class AddExtraScopesOnRatingRatesTable < ActiveRecord::Migration[5.0]
     add_column :rating_rates, :scope_1, :string
     add_column :rating_rates, :scope_2, :string
 
-    remove_index :rating_rates, %i[author_id author_type resource_id resource_type scopeable_id scopeable_type]
+    # remove_index :rating_rates, %i[author_id author_type resource_id resource_type scopeable_id scopeable_type]
   end
 end
