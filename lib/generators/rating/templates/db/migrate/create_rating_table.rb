ad0a49abec51b255c8ef780eda0882381f4c27d2
Washington Botelho <wbotelhos@gmail.com>
2026-04-27 11:24:11 -0300

    sch: uses 12 8 to avoid overflow on integer count

--- lib/generators/rating/templates/db/migrate/create_rating_table.rb
+++ lib/generators/rating/templates/db/migrate/create_rating_table.rb
@@ -3,10 +3,10 @@
 class CreateRatingTable < ActiveRecord::Migration[5.0]
   def change
     create_table :rating_ratings do |t|
-      t.decimal :average,  default: 0, mull: false, precision: 11, scale: 2
-      t.decimal :estimate, default: 0, mull: false, precision: 11, scale: 2
-      t.integer :sum,      default: 0, mull: false
-      t.integer :total,    default: 0, mull: false
+      t.decimal :average,  default: 0, null: false, precision: 12, scale: 8
+      t.decimal :estimate, default: 0, null: false, precision: 12, scale: 8
+      t.integer :sum,      default: 0, null: false
+      t.integer :total,    default: 0, null: false
 
       t.references :resource,  index: true, null: false, polymorphic: true
       t.references :scopeable, index: true, null: true,  polymorphic: true
